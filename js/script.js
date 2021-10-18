be3d0205ec418b462c05b83e5fbad405c8d45f06
Daniel <exemple@gmail.com>
2021-10-19 00:35:20 +0300

    lesson 15

--- js/script.js
+++ js/script.js
@@ -1,67 +1,88 @@
 "use strict";
 
-//const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
 
-//const personalMovieDB = {
-//	count: numberOfFilms,
-//	movies: {},
-//	actors: {},
-//	genres: [],
-//	privat: false
-//};
-//
-//let a = prompt("Один из последних просмотренных фильмов?"),
-//	 b = prompt("На сколько оцените его?"),
-//	 c = prompt("Один из последних просмотренных фильмов?"),
-//	 d = prompt("На сколько оцените его?");
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
 
-//personalMovieDB.movies[a] = b;
-//personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+	const a = prompt("Один из последних просмотренных фильмов?"),
+			b = prompt("На сколько оцените его?");
 
-//console.log(personalMovieDB);
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log('done');
+	} else {
+		console.log('error');
+		i--;
+	}
+	
+	if (personalMovieDB.count < 10) {
+		console.log('Просмотрено довольно мало фильмов');
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+		console.log('Вы классический зритель');
+	} else if (personalMovieDB.count > 50) {
+		console.log('Вы киноман');
+	} else {
+		console.log('Произошла ошибка');
+	}
 
-const RusikWaight = 70;
-
-if (RusikWaight < 80) {
-	console.log('Ты в хорошей форме!');
-} else if (RusikWaight > 86){
-	console.log('Ты жирный хуй!');
-} else {
-	console.log('Нормальный вес');
 }
 
-let num = 1; 
 
-switch (num) {
-	case 49: 
-		console.log('Не правильно');
-	break;
-	case 60: 
-		console.log('Не правильно');
-	break;
-	case 50:
-		console.log('правильно');
-		break;
-	default: 
-	console.log('В след раз');
-	break;
-}
+console.log(personalMovieDB);
 
-let b = 1;
+
+
+
+// const RusikWaight = 70;
+
+// if (RusikWaight < 80) {
+// 	console.log('Ты в хорошей форме!');
+// } else if (RusikWaight > 86){
+// 	console.log('Ты жирный хуй!');
+// } else {
+// 	console.log('Нормальный вес');
+// }
+
+// let num = 1; 
+
+// switch (num) {
+// 	case 49: 
+// 		console.log('Не правильно');
+// 	break;
+// 	case 60: 
+// 		console.log('Не правильно');
+// 	break;
+// 	case 50:
+// 		console.log('правильно');
+// 		break;
+// 	default: 
+// 	console.log('В след раз');
+// 	break;
+// }
+
+// let b = 1;
 //while (b <= 50) {
 //	console.log(b);
 //	b++;	
 //}
 
-do {
-	console.log(b);
-	b++;	
-}
-while (b <= 50);
+// do {
+// 	console.log(b);
+// 	b++;	
+// }
+// while (b <= 50);
 
-for (let i = 1; i < 33; i++) {
-	if (i === 19) {
-		continue;
-	}
-	console.log(i);
-}
+// for (let i = 1; i < 33; i++) {
+// 	if (i === 19) {
+// 		continue;
+// 	}
+// 	console.log(i);
+// }
+
